61848278b2fdde4b15a6c32f0413662e1875a468
Daiana Michels <daianaem.12@gmail.com>
2023-10-19 21:44:49 -0300

    dynamically populating cards

---
+++ data/dataFake.ts
@@ -0,0 +1,46 @@
+export const dataFake = {
+  games: [
+    {
+      id: 1,
+      gameCover: '../../../assets/ac-cover.jpg',
+      gameLabel: 'Exclusive',
+      gameType: 'DIGITAL PS5',
+      gamePrice: '$45,00',
+    },
+    {
+      id: 2,
+      gameCover: '../../../assets/bt-1.jpg',
+      gameLabel: 'Latest units',
+      gameType: 'PHYSICAL MEDIA PS4',
+      gamePrice: '$32,00',
+    },
+    {
+      id: 3,
+      gameCover: '../../../assets/bt-4.jpg',
+      gameLabel: 'Exclusive',
+      gameType: 'DIGITAL PS4',
+      gamePrice: '$50,00',
+    },
+    {
+      id: 4,
+      gameCover: '../../../assets/bt-5.jpg',
+      gameLabel: 'Latest units',
+      gameType: 'PHYSICAL MEDIA PS4',
+      gamePrice: '$57,00',
+    },
+    {
+      id: 5,
+      gameCover: '../../../assets/bt-bad-company-2.jpg',
+      gameLabel: 'Exclusive',
+      gameType: 'DIGITAL PS4 | PS5',
+      gamePrice: '$75,00',
+    },
+    {
+      id: 6,
+      gameCover: '../../../assets/bt-hardline.jpg',
+      gameLabel: 'New',
+      gameType: 'DIGITAL PS5',
+      gamePrice: '$62,00',
+    }
+  ],
+};
